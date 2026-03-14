69c1ad85d8c010c9c1581f1a429e71447b9521c8
joshua <joshuasunday300@gmail.com>
2026-03-15 00:30:09 +0100

    chore: updated buffer error warning

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Buffer } from "buffer";
+
+window.Buffer = Buffer;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
